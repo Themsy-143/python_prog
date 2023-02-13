0926565fc8f7e700330aa4df9bd8efbbdad6d642
Themsy-143 <themsyparmar3014@gmail.com>
2023-02-13 15:32:52 +0530

    search book

--- books.py
+++ books.py
@@ -22,6 +22,12 @@ def chnge_prize(list,c_book_name,c_prize):
             i["Book_prize"]= c_prize
     return list
 
+def search_book(list,s_book):
+    for i in list:
+        if i["book_name"] == s_book:
+            print("search book: ",i["book_name"],i["Author_name"],i["Book_prize"])
+            print(i)
+
 n = int(input("How many books do you have?: "))
 list = []
 for i in range(0,n):
@@ -33,7 +39,7 @@ for i in range(0,n):
     list.append(book_entry)
 print(list)
 
-choice = input("What do you want to do? \nadd book = a \ndelete book = d \nchange prize = p \n---: ")
+choice = input("What do you want to do? \nadd book = a \ndelete book = d \nchange prize = p \nsearch book = s\n---: ")
 
 if (choice == 'a'):
     list = ad_book(list)
@@ -50,3 +56,10 @@ elif (choice == 'p'):
     list = chnge_prize(list,c_book_name,c_prize)
     print("changed prize: ",list)
 
+elif (choice =='s'):
+    s_book = input("Enter book name you want to search: ")
+    search_book(list,s_book)
+    # print("searched books: ",list)
+    
+else:
+    print("WRONG CHOICE....")
